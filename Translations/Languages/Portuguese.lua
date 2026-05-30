043b0338508a7e14d34d706f7ccd7639833804a4
Astral Dev 2 <lucasviniciussoaresdasilva100@gmail.com>
2026-05-29 21:30:43 -0300

    Add files via upload

--- Translations/Languages/Portuguese.lua
+++ Translations/Languages/Portuguese.lua
@@ -1,5 +1,16 @@
 return {
 	["994732206"] = {
-			
+		["Farm"] = "Farmar",
+		["Items/Quests"] = "Itens/Missões",
+		["Islands"] = "Ilhas",
+		["Sea Events"] = "Eventos Marítimos",
+		["Fruits/Raid"] = "Frutas/Raid",
+		["Teleport"] = "Teletransporte",
+		["Stats"] = "Estatísticas",
+		["Race"] = "Raça",
+		["Player"] = "Jogador",
+		["Shop"] = "Comprar",
+		["Visual"] = "Visual",
+		["Settings"] = "Configurações",
 	}
 }
