b6b7081f6d755c64c2b93223e6b9876ff20ad9fe
acreativeandcoolname <lucasviniciussoaresdasilva100@gmail.com>
2026-08-05 20:03:46 -0300

    Add Material.lua with item and enemy data

---
+++ Games/BloxFruits/Material.lua
@@ -0,0 +1,44 @@
+return {
+ List = {
+  {
+   "Angel Wings",
+   "Fish Tail",
+   "Leather + Scrap Metal",
+   "Magma Ore",
+  },
+  {
+   "Leather + Scrap Metal",
+   "Magma Ore",
+   "Mystic Doplet",
+   "Radioctive Material",
+   "Vampire Fang"
+  },
+  {
+   "Conjured Cocoa",
+   "Demonic Wisp",
+   "Dragon Scale",
+   "Fish Tail",
+   "Gunpowder",
+   "Leather + Scrap Metal",
+   "Mini Tusk",
+   "Nightmare Catcher",
+   "Volt Capsule"
+  },
+ },
+ Enemies = {
+  ["Angel Wings"] = {{"God's Guard", "Shanda", "Royal Squad", "Royal Soldier", "Wysper", "Thunder God"}},
+  ["Conjured Cocoa"] = {false, false, {"Cocoa Warrior", "Chocolate Bar Battler"}},
+  ["Demonic Wisp"] = {false, false, {"Demonic Soul"}},
+  ["Dragon Scale"] = {false, false, {"Dragon Crew Warrior", "Dragon Crew Archer"}},
+  ["Fish Tail"] = {{"Fishman Warrior", "Fishman Commando", "Fishman Lord"}, false, {"Fishman Raider", "Fishman Captain"}},
+  ["Gunpowder"] = {false, false, {"Pistol Billionaire"}},
+  ["Leather + Scrap Metal"] = {{"Pirate","Brute"}, {"Mercenary"}, {"Pirate Millionaire","Pistol Billionaire"}},
+  ["Magma Ore"] = {{"Military Soldier", "Military Spy", "Magma Admiral"}, {"Magma Ninja", "Lava Pirate"}},
+  ["Mini Tusk"] = {false, false, {"Floating Turtle"}},
+  ["Mystic Doplet"] = {false, {"Sea Soldier", "Water Fighter"}},
+  ["Nightmare Catcher"] = {false, false, {"Reborn Skeleton", "Living Zombie"}},
+  ["Radioctive Material"] = {false, {"Factory Staff"}},
+  ["Vampire Fang"] = {false, {"Vampire"}},
+  ["Volt Capsule"] = {false, false, {"Sea Chanter", "Ocean Prophet"}},
+ },
+}
